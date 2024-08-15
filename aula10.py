9a7dd82efa85a779f78eea61c9e995c3e1084972
Geisy Viana <geisyviana5@gmail.com>
2024-08-14 23:11:59 -0300

    atualização dos estudos.

---
+++ aula10.py
@@ -0,0 +1,8 @@
+concatenacao = 'A' + 'B' + 'C' + '\n' + 'Geisy' + ' ' + 'Viana'
+print(concatenacao)
+
+a_dez_vezes = 'A' * 10
+tres_vezes_geisy = 3 * 'Geisy '
+print(a_dez_vezes)
+print(tres_vezes_geisy)
+
---
+++ aula7.py
@@ -0,0 +1,18 @@
+# Variáveis são usadas para salvar algo na memória do computador.
+# PEP8: inicie variáveis com letras minúsculas, pode usar
+# números e underline _.
+# O sinal de = é o operador de atribuição. Ele é usado para
+# atribuir um valor a um nome (variável).
+# Uso: nome_variavel = expressão
+
+nome_completo = 'Geisy Viana'
+soma_dois_mais_dois = 2 + 2
+print(nome_completo, soma_dois_mais_dois)
+bool_um = bool ('1')
+print(bool_um)
+print("\n")
+nome = 'Geisy'
+idade = 30
+maior_de_idade = idade >= 18
+print('Nome: ', nome, '\n','Idade: ', idade)
+print('É maior? ', maior_de_idade)
