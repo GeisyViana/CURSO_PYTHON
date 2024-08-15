64b0e0000b882c73941bd4dbf789ea0466135694
Geisy Viana <geisyviana5@gmail.com>
2024-08-14 21:51:29 -0300

    changes

---
+++ aula3.py
@@ -0,0 +1,21 @@
+"""
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Geisy Viana')
+print(1,'Geisy "Viana"')
+
+# Aspas duplas
+print("Geisy Viana")
+print(2,"Geisy 'Viana'")
+
+# Escape
+print("Geisy \"Viana\"")
+
+# r
+print(r"Geisy \"Viana\"")
